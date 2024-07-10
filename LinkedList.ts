03ba28f058435716e0b21965311a0804559e15fd
Ramon Saviato <ramonsaviato@hotmail.com>
2024-07-09 22:46:16 -0300

    implement at

--- LinkedList.ts
+++ LinkedList.ts
@@ -26,22 +26,18 @@ export class LinkedList<T> {
 
     append(value: T) {
         const node = new ListNode(value);
-
         if(!this._tail) {
             this._head = this._tail = node;
         }
         else{
             this._tail = this._tail.next = node;
         }
-
         this.length++;
-
         return this;
     }
 
     prepend(value: T) {
         const node = new ListNode(value);
-
         if(!this._head) {
             this._head = this._tail = node;
         }
@@ -49,9 +45,7 @@ export class LinkedList<T> {
             node.next = this._head;
             this._head = node;
         }
-
         this.length++;
-
         return this;
     }
 
@@ -68,7 +62,14 @@ export class LinkedList<T> {
     }
 
     at(index: number) {
-
+        if(index < 0 || index >= this.length) return null;
+        if(index === this.length - 1) return this._tail!.value;
+        let node = this._head;
+        while(index > 0) {
+            node = node!.next;
+            index--;
+        }
+        return node!.value;
     }
 
     insertAt(value: T, index: number) {
